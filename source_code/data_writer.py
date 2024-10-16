dc7638a38e56a98932e3c190bea8a1a3bc75bbbc
Nikita <niklim01@mail.ru>
2024-10-16 13:19:00 +0300

    Change python files to check pre-commit

--- source_code/data_writer.py
+++ source_code/data_writer.py
@@ -1,18 +1,16 @@
 import json
+
 from dict2xml import dict2xml
 
 
-
-
 class DataWriter:
-    def __init__(self,write_folder):
+    def __init__(self, write_folder):
         self.path_to_write = write_folder
 
-
     def write_info(self, file_name, output_format, info):
-        if output_format == 'json':
-            result = json.dumps(info,indent=4)
-        if output_format == 'xml':
-            result = dict2xml(info,indent ="   ")
-        with open(self.path_to_write+file_name,'w') as res_file:
+        if output_format == "json":
+            result = json.dumps(info, indent=4)
+        if output_format == "xml":
+            result = dict2xml(info, indent="   ")
+        with open(self.path_to_write + file_name, "w") as res_file:
             res_file.write(result)
--- test/unittests.py
+++ test/unittests.py
@@ -1,66 +1,92 @@
-import unittest
-from unittest.mock import patch, mock_open
-from source_code.arg_parser import ArgParserManager
-from source_code.db_manager import DatabaseManager
-from source_code.data_writer import DataWriter 
 import json
+import unittest
+from unittest.mock import mock_open, patch
+
+from source_code.arg_parser import ArgParserManager
+from source_code.data_writer import DataWriter
+from source_code.db_manager import DatabaseManager
+
 
 class ArgumentParserManager(unittest.TestCase):
-    @patch('sys.argv', ['program', '-r', 'json//rooms.json', '-s', 'json//students.json', '-f', 'xml'])
+    @patch(
+        "sys.argv",
+        [
+            "program",
+            "-r",
+            "json//rooms.json",
+            "-s",
+            "json//students.json",
+            "-f",
+            "xml",
+        ],
+    )
     def test_parse_arguments(self):
         parser = ArgParserManager()
         args = parser.get_arguments()
-        self.assertEqual(args.r, 'json//rooms.json')
-        self.assertEqual(args.s, 'json//students.json')
-        self.assertEqual(args.f, 'xml')
+        self.assertEqual(args.r, "json//rooms.json")
+        self.assertEqual(args.s, "json//students.json")
+        self.assertEqual(args.f, "xml")
 
-    @patch('sys.argv', ['program'])
+    @patch("sys.argv", ["program"])
     def test_parse_arguments_no_arguments(self):
         parser = ArgParserManager()
         args = parser.get_arguments()
         self.assertIsNone(args)
 
-    ('sys.argv', ['program', '-r', 'json//rooms.json', '-s', 'json//students.json', '-f', 'xmml'])
+    (
+        "sys.argv",
+        [
+            "program",
+            "-r",
+            "json//rooms.json",
+            "-s",
+            "json//students.json",
+            "-f",
+            "xmml",
+        ],
+    )
+
     def test_parse_arguments_not_valid_arguments(self):
         parser = ArgParserManager()
         is_valid = parser.validate_arguments()
-        self.assertEqual(is_valid,False)
-
+        self.assertEqual(is_valid, False)
 
 
 class TestDatabaseManager(unittest.TestCase):
-    @patch('mysql.connector.connect')
+    @patch("mysql.connector.connect")
     def test_connection(self, mock_connect):
         config = {
-            'host': 'localhost',
-            'user': 'root',
-            'password': 'Nekito2001',
-            'sql_folder': 'sql//'
-        } 
-        db_manager = DatabaseManager(config)
-        mock_connect.assert_called_with(host=config['host'], user=config['user'], password=config['password'])
-
+            "host": "localhost",
+            "user": "root",
+            "password": "Nekito2001",
+            "sql_folder": "sql//",
+        }
+        DatabaseManager(config)
+        mock_connect.assert_called_with(
+            host=config["host"],
+            user=config["user"],
+            password=config["password"],
+        )
 
     def test_read_sql(self):
         config = {
-            'host': 'localhost',
-            'user': 'root',
-            'password': 'Nekito2001',
-            'sql_folder': 'sql//'
-        } 
+            "host": "localhost",
+            "user": "root",
+            "password": "Nekito2001",
+            "sql_folder": "sql//",
+        }
         db_manager = DatabaseManager(config)
-        sql_queries = db_manager.read_sql('create.sql')
-        self.assertIsInstance(sql_queries,list)
+        sql_queries = db_manager.read_sql("create.sql")
+        self.assertIsInstance(sql_queries, list)
 
-
-    @patch('mysql.connector.connect')
+    @patch("mysql.connector.connect")
     def test_execute_query(self, mock_connect):
         config = {
-            'host': 'localhost',
-            'user': 'root',
-            'password': 'Nekito2001',
-            'sql_folder': 'sql//'
-        }  
+            "host": "localhost",
+            "user": "root",
+            "password": "Nekito2001",
+            "sql_folder": "sql//",
+        }
         mock_connection = mock_connect.return_value
         mock_cursor = mock_connection.cursor.return_value
 
@@ -70,14 +96,14 @@ class TestDatabaseManager(unittest.TestCase):
         mock_cursor.execute.assert_called_with("SELECT * FROM student")
         mock_connection.commit.assert_called_once()
 
-    @patch('mysql.connector.connect')
+    @patch("mysql.connector.connect")
     def test_close_connection(self, mock_connect):
         config = {
-            'host': 'localhost',
-            'user': 'root',
-            'password': 'Nekito2001',
-            'sql_folder': 'sql//'
-        } 
+            "host": "localhost",
+            "user": "root",
+            "password": "Nekito2001",
+            "sql_folder": "sql//",
+        }
         mock_connection = mock_connect.return_value
 
         db_manager = DatabaseManager(config)
@@ -86,36 +112,22 @@ class TestDatabaseManager(unittest.TestCase):
         mock_connection.close.assert_called_once()
 
 
-
-
-
-
-
-
-
-
-
-
-
 class TestDataWriter(unittest.TestCase):
-    @patch('builtins.open', new_callable=mock_open)
-    def test_write_info(self,mock_file):
-        path_to_write = 'res//json//'
-        file_name = 'query1.html'
-        output_format = 'json'
-        data = [{'id': 1, 'name': 'Nikita'}, {'id': 2, 'name': 'Alina'} ]
+    @patch("builtins.open", new_callable=mock_open)
+    def test_write_info(self, mock_file):
+        path_to_write = "res//json//"
+        file_name = "query1.html"
+        output_format = "json"
+        data = [{"id": 1, "name": "Nikita"}, {"id": 2, "name": "Alina"}]
         writer = DataWriter(path_to_write)
-        writer.write_info(file_name,output_format, data)
+        writer.write_info(file_name, output_format, data)
 
-
-        mock_file.assert_called_once_with(path_to_write+file_name, 'w', encoding = 'utf-8')
-        result = json.dumps(data,indent=4)
+        mock_file.assert_called_once_with(
+            path_to_write + file_name, "w", encoding="utf-8"
+        )
+        result = json.dumps(data, indent=4)
         mock_file().write.assert_called_once_with(result)
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-        
